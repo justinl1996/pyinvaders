73561b8153aac3fe05636d7c6bfb2763b026a3f1
justin <justinl1996@yahoo.com>
2015-02-12 10:29:11 +1000

    New TextMsg

---
+++ data/textmsg.py
@@ -0,0 +1,12 @@
+__author__ = 'justin'
+
+class NewWeapon:
+    rocket = "Rocket Launcher Obtained"
+    dual = "Dual Rail Gun Attachment Obtained"
+    spread = "Wide Shot Canon Obtained"
+
+class Max:
+    rocket = "Rockets at Max"
+    ammo = "Ammo at Max"
+    shield = "Shield at Max"
+
